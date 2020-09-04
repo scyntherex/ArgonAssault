bdc5e6603b9e4d648f20c7adfcff04f8be51514d
Rone Caballero <rone.caballero@gmail.com>
2020-09-03 21:34:54 -0700

    Ship now moves left to right of screen.

--- RailsShooter/Assets/Player.cs
+++ RailsShooter/Assets/Player.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +18,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontalThrow);
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffSet = xThrow * xSpeed * Time.deltaTime;
+
+        float rawNewXPos = transform.localPosition.x + xOffSet;
+        float clampedXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+
     }
 }
